362353af4333ef1685164d080075166d14d48165
b1thunt3r <ishan@jain.se>
2022-06-24 11:36:39 +0100

    Initial Commit

---
+++ src/core/Bit0.CrunchLog.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bit0.Crunchlog.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
